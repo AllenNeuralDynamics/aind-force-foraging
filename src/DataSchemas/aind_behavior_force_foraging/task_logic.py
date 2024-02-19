2a747f2161d510240bca3dec548f36ac4ce9b8a2
bruno-f-cruz <bfpcruz@gmail.com>
2024-02-19 06:53:06 -0800

    wip

--- src/DataSchemas/aind_behavior_force_foraging/task_logic.py
+++ src/DataSchemas/aind_behavior_force_foraging/task_logic.py
@@ -23,6 +23,70 @@ def scalar_value(value: float) -> distributions.Scalar:
     return distributions.Scalar(distribution_parameters=distributions.ScalarDistributionParameter(value=value))
 
 
+class UpdateTrigger(str, Enum):
+    TIME = 'Time'
+    TRIAL = 'Trial'
+    REWARD = 'Reward'
+
+
+class _UpdateFunction(AindModel):
+    function_type: str
+    update_on: UpdateTrigger = Field(default=UpdateTrigger.TRIAL)
+class PowerFunction(_UpdateFunction):
+    function_type: Literal["PowerFunction"] = "PowerFunction"
+    mininum: float = Field(default=0, description="Minimum value of the function")
+    maximum: float = Field(default=1, description="Maximum value of the function")
+    a: float = Field(default=1, description="Coefficient a of the function: value = a * pow(b, c * x) + d")
+    b: float = Field(
+        default=2.718281828459045, description="Coefficient b of the function: value = a * pow(b, c * x) + d"
+    )
+    c: float = Field(default=-1, description="Coefficient c of the function: value = a * pow(b, c * x) + d")
+    d: float = Field(default=0, description="Coefficient d of the function: value = a * pow(b, c * x) + d")
+
+
+class LinearFunction(_UpdateFunction):
+    function_type: Literal["LinearFunction"] = "LinearFunction"
+    mininum: float = Field(default=0, description="Minimum value of the function")
+    maximum: float = Field(default=9999, description="Maximum value of the function")
+    a: float = Field(default=1, description="Coefficient a of the function: value = a * x + b")
+    b: float = Field(default=0, description="Coefficient b of the function: value = a * x + b")
+
+
+class ConstantFunction(_UpdateFunction):
+    function_type: Literal["ConstantFunction"] = "ConstantFunction"
+    value: float = Field(default=1, description="Value of the function")
+
+class LookUpTable(_UpdateFunction):
+    function_type: Literal["LookUpTable"] = "LookUpTable"
+    values: List[float] = Field(..., min_length=1)
+    is_repeat: bool = Field(default=False)
+    is_shuffle: bool = Field(default=False)
+
+class UpdateFunction(RootModel):
+    root: Annotated[Union[ConstantFunction, LinearFunction, PowerFunction, LookUpTable], Field(discriminator="function_type")]
+
+
+
+class HarvestAction(BaseModel):
+    probability: UpdateFunction = Field(default=ConstantFunction(value=1), description="Probability of reward", validate_default=True)
+    amount: UpdateFunction = Field(default=ConstantFunction(value=1), description="Amount of reward to be delivered", validate_default=True)
+    delay: UpdateFunction = Field(default=ConstantFunction(value=0), description="Delay between sucessful harvest and reward delivery", validate_default=True)
+    press_duration: UpdateFunction = Field(default=ConstantFunction(value=1), description="Duration that the force much stay above threshold", validate_default=True)
+    press_force_threshold: UpdateFunction = Field(default=ConstantFunction(value=10_000), description="Force to be applied", validate_default=True)
+
+
+class Block(BaseModel):
+    left_action: HarvestAction = Field(..., validate_default=True, description="Specification of the left action")
+    right_action: HarvestAction = Field(..., validate_default=True, description="Specification of the right action")
+    block_size: distributions.Distribution = scalar_value(20)
+
+
+
+
+class EnvironmentStatistics(BaseModel):
+
+
+
 class AindForceForagingTaskLogic(AindBehaviorTaskLogicModel):
     describedBy: str = Field("")
     schema_version: Literal[__version__] = __version__
