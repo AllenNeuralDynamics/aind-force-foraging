44a00c284b39a5611f6b977b4c4e824e89a3399c
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-02-20 17:09:12 -0800

    Refactor block structure

--- src/DataSchemas/aind_behavior_force_foraging/task_logic.py
+++ src/DataSchemas/aind_behavior_force_foraging/task_logic.py
@@ -23,6 +23,20 @@ def scalar_value(value: float) -> distributions.Scalar:
     return distributions.Scalar(distribution_parameters=distributions.ScalarDistributionParameter(value=value))
 
 
+def uniform_distribution_value(min: float, max: float) -> distributions.Uniform:
+    """
+    Helper function to create a uniform distribution for a given range.
+
+    Args:
+        min (float): The minimum value of the uniform distribution.
+        max (float): The maximum value of the uniform distribution.
+
+    Returns:
+        distributions.Uniform: The uniform distribution type.
+    """
+    return distributions.UniformDistribution(distribution_parameters=distributions.UniformDistributionParameters(max=max, min=min))
+
+
 ## Available actions
 class HarvestActionBase(BaseModel):
     '''Defines an abstract class for an harvest action'''
@@ -54,23 +68,48 @@ class Trial(BaseModel):
     time_out: float = Field(default=0, ge=0, description="Time out for the trial")
 
 
+class EnvironmentStatisticsMode(str, Enum):
+    '''Defines the mode of the environment'''
+    BLOCK = "Block"
+    RANDOMWALK = "RandomWalk"
+    BLOCKGENERATOR = "BlockGenerator"
+
+
 class Block(BaseModel):
+    mode: Literal[EnvironmentStatisticsMode.BLOCK] = EnvironmentStatisticsMode.BLOCK
     trials: List[Trial] = Field(default=[], description="List of trials in the block")
     shuffle: bool = Field(default=False, description="Whether to shuffle the trials in the block")
     repeat_count: Optional[int] = Field(default=0, description="Number of times to repeat the block. If null, the block will be repeated indefinitely")
 
 
-class EnvironmentStatisticsMode(str, Enum):
-    '''Defines the mode of the environment'''
-    BLOCK = "Block"
-    RANDOMWALK = "RandomWalk"
+class BlockGenerator(BaseModel):
+    mode: Literal[EnvironmentStatisticsMode.BLOCKGENERATOR] = EnvironmentStatisticsMode.BLOCKGENERATOR
+    baited: bool = Field(default=False, description="Whether the trials are baited")
+    block_size: distributions.Distribution = Field(default=uniform_distribution_value, validate_default=True, description="Size of the block")
+    end_block_criteria: Optional[EndBlockCriteria] = Field(default=None, description="Criteria to end the block")
 
 
-class BlockedStatistics(BaseModel):
-    mode: Literal[EnvironmentStatisticsMode.BLOCK] = EnvironmentStatisticsMode.BLOCK
-    blocks: List[Block] = Field(default=[], description="List of blocks in the environment")
-    shuffle: bool = Field(default=False, description="Whether to shuffle the blocks in the environment")
-    repeat_count: Optional[int] = Field(default=0, description="Number of times to repeat the environment. If null, the environment will be repeated indefinitely")
+class _EndBlockCriteriaBase(BaseModel):
+    criteria: str
+
+
+class EndBlockCriteria(RootModel):
+    root: Annotated[Union[RewardRateCriteria, PerformanceCriteria, TimeCriteria], Field(discriminator='criteria')]
+
+
+class RewardRateCriteria(_EndBlockCriteriaBase):
+    criteria: Literal['RewardRate'] = 'RewardRate'
+    threshold: float = Field(..., description="Reward rate to end the block")
+
+
+class PerformanceCriteria(_EndBlockCriteriaBase):
+    criteria: Literal['RewardRate'] = 'RewardRate'
+    threshold: float = Field(..., description="Reward rate to end the block")
+
+
+class TimeCriteria(_EndBlockCriteriaBase):
+    criteria: Literal['Time'] = 'Time'
+    duration_threshold: float = Field(..., description="Duration to end the block", ge=0)
 
 
 class RandomWalkStatistics(BaseModel):
@@ -78,11 +117,13 @@ class RandomWalkStatistics(BaseModel):
 
 
 class EnvironmentStatistics(RootModel):
-    root: Annotated[Union[BlockedStatistics, RandomWalkStatistics], Field(discriminator='mode')]
+    root: Annotated[Union[Block, RandomWalkStatistics], Field(discriminator='mode')]
 
 
 class Environment(BaseModel):
-    statistics: EnvironmentStatistics = Field(..., description="Statistics of the environment")
+    block_statistics: List[EnvironmentStatistics] = Field(..., description="Statistics of the environment")
+    shuffle: bool = Field(default=False, description="Whether to shuffle the blocks")
+    repeat_count: Optional[int] = Field(default=0, description="Number of times to repeat the environment. If null, the environment will be repeated indefinitely")
 
 
 # Updaters
