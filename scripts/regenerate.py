23177d5afdf0965a0e6d2c75535f898336a58ecf
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-05-10 16:38:24 -0700

    Refactor model definition to follow the latest services package

--- scripts/regenerate.py
+++ scripts/regenerate.py
@@ -2,8 +2,8 @@ import inspect
 from pathlib import Path
 
 import aind_behavior_force_foraging.rig
-import aind_behavior_force_foraging.session
 import aind_behavior_force_foraging.task_logic
+from aind_behavior_services.session import AindBehaviorSessionModel
 from aind_behavior_services.utils import convert_pydantic_to_bonsai, pascal_to_snake_case, snake_to_pascal_case
 
 SCHEMA_ROOT = Path("./src/DataSchemas/")
@@ -14,9 +14,9 @@ NAMESPACE_PREFIX = "AindForceForagingDataSchema"
 def main():
 
     models = [
-        aind_behavior_force_foraging.task_logic.schema(),
-        aind_behavior_force_foraging.session.schema(),
-        aind_behavior_force_foraging.rig.schema(),
+        aind_behavior_force_foraging.task_logic.AindForceForagingTaskLogic,
+        aind_behavior_force_foraging.rig.AindForceForagingRig,
+        AindBehaviorSessionModel
     ]
 
     for model in models:
--- src/DataSchemas/aind_behavior_force_foraging/rig.py
+++ src/DataSchemas/aind_behavior_force_foraging/rig.py
@@ -21,9 +21,13 @@ class RigCalibration(BaseModel):
 
 
 class AindForceForagingRig(AindBehaviorRigModel):
-    schema_version: Literal[__version__] = __version__
-    auxiliary_camera0: Optional[rig.WebCamera] = Field(default=rig.WebCamera(index=0), description="Auxiliary camera 0")
-    auxiliary_camera1: Optional[rig.WebCamera] = Field(default=rig.WebCamera(index=1), description="Auxiliary camera 1")
+    version: Literal[__version__] = __version__
+    triggered_camera_controller: rig.CameraController[rig.SpinnakerCamera] = Field(
+        ..., description="Required camera controller to triggered cameras."
+    )
+    monitoring_camera_controller: Optional[rig.CameraController[rig.WebCamera]] = Field(
+        default=None, description="Optional camera controller for monitoring cameras."
+    )
     harp_behavior: rig.HarpBehavior = Field(..., description="Harp behavior")
     harp_olfactometer: Optional[rig.HarpOlfactometer] = Field(None, description="Harp olfactometer")
     harp_sniff_detector: Optional[rig.HarpSniffDetector] = Field(None, description="Harp sniff detector")
@@ -31,12 +35,5 @@ class AindForceForagingRig(AindBehaviorRigModel):
     harp_load_cells: rig.HarpLoadCells = Field(..., description="Harp load cells")
     harp_clock_generator: rig.HarpClockGenerator = Field(..., description="Harp clock generator")
     harp_analog_input: Optional[rig.HarpAnalogInput] = Field(default=None, description="Harp analog input")
-    face_camera: rig.SpinnakerCamera = Field(..., description="Face camera")
-    top_body_camera: Optional[rig.SpinnakerCamera] = Field(default=None, description="Top body camera")
-    side_body_camera: Optional[rig.SpinnakerCamera] = Field(default=None, description="Side body camera")
     screen: rig.Screen = Field(default=rig.Screen(), description="Screen settings")
     calibration: RigCalibration = Field(default=None, description="Load cells calibration")
-
-
-def schema() -> BaseModel:
-    return AindForceForagingRig
