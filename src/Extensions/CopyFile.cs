80421758d365aecf277b78b8cbdcb22a27e15fbf
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-08-20 09:59:12 -0700

    Add generic operator to copy files

---
+++ src/Extensions/CopyFile.cs
@@ -0,0 +1,43 @@
+using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using System.IO;
+
+[Combinator]
+[Description("Copies a target file.")]
+[WorkflowElementCategory(ElementCategory.Sink)]
+public class CopyFile
+{
+    private string sourcePath;
+    [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
+    [Description("Name of the source file to be copied.")]
+    public string SourcePath
+    {
+        get { return sourcePath; }
+        set { sourcePath = value; }
+    }
+
+    private string destinationPath;
+    [Editor("Bonsai.Design.SaveFileNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
+    [Description("Name of the destination file.")]
+    public string DestinationPath
+    {
+        get { return destinationPath; }
+        set { destinationPath = value; }
+    }
+
+    private bool overwrite;
+    public bool Overwrite
+    {
+        get { return overwrite; }
+        set { overwrite = value; }
+    }
+
+    public IObservable<T> Process<T>(IObservable<T> source)
+    {
+        return source.Do(_ => {
+            File.Copy(sourcePath, destinationPath, overwrite);
+        });
+    }
+}
