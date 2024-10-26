ac71d13c8651cdb62da642eda0e15cc84fc6d2eb
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-10-26 13:52:41 -0700

    Move regenerate script to project.toml

--- src/Extensions/AindForceForagingTaskLogic.cs
+++ src/Extensions/AindForceForagingTaskLogic.cs
@@ -127,7 +127,7 @@ namespace AindForceForagingDataSchema.TaskLogic
     
         private double? _rngSeed;
     
-        private string _aindBehaviorServicesPkgVersion = "0.8.1";
+        private string _aindBehaviorServicesPkgVersion = "0.8.2";
     
         private Environment _environment = new Environment();
     
